b699994d0f415bdaa8521d0d52edb47630a1a133
Mykhailo <cbo6oda22@gmail.com>
2026-06-12 23:00:18 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,18 +1,21 @@
 from app.main import check_password
-import pytest
 
 
-def test_min_password_length():
+def test_min_password_length() -> None:
     assert check_password("Pass@11") == False
 
-def test_max_password_length():
+
+def test_max_password_length() -> None:
     assert check_password("P@ssword123_P@ss1") == False
 
-def test_contains_special_characters():
+
+def test_contains_special_characters() -> None:
     assert check_password("Password1") == False
 
-def test_password_contains_digits():
+
+def test_password_contains_digits() -> None:
     assert check_password("P@ssword") == False
 
-def test_password_contains_uppercase():
+
+def test_password_contains_uppercase() -> None:
     assert check_password("p@ssword123_") == False
